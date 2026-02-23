caa541c223ae617f0e08374e840211f086c00b28
Elliot-Fry <elioconpraro@gmail.com>
2026-02-23 12:37:02 +1100

    Scraping #2

--- Event-Scraping/scraper.py
+++ Event-Scraping/scraper.py
@@ -1,7 +1,35 @@
 import csv
+import requests
+from lxml import html
 
-with open('event_links.csv',newline='') as csvf:
+# Scraping guide: https://docs.python-guide.org/scenarios/scrape/
+
+def get_bracket_link(eventName):
+    # FUNCTION get_bracket_link
+    # Extracts the link to an tournament's bracket from the event page
+    
+    prefix = 'https://www.start.gg/tournament/'
+    suffix = '/events/singles/brackets'
+    url = prefix + eventName + suffix
+    
+    print('Accessing URL: ', url)
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    anchor = tree.xpath('//div[@class="phaseRows-sggOQ3xu"]')
+     
+    href = tree.xpath('//div[@class="phaseRows-sggOQ3xu"]/@href')
+
+    print(anchor)
+    print(href)
+    print('\n\n___PAGE HTML: ___\n', page.content[:2000])
+    
+
+
+
+with open('Event-Scraping\event_links.csv',newline='') as csvf:
     reader = csv.reader(csvf)
+    header = next(reader,None)
     for row in reader:
-        print(', '.join(row))
-
+        bracketLink = get_bracket_link(row[0])
+        break
+        
